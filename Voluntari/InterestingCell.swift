7cd7c55dcffd8b06dfed578308ce92330ceeb3b5
tonyyasi <tonyyasi@gmail.com>
2016-08-27 17:11:15 -0500

    Adds the interesting cell class and applies it to the cell

---
+++ Voluntari/InterestingCell.swift
@@ -0,0 +1,13 @@
+//
+//  InterestingCell.swift
+//  Voluntari
+//
+//  Created by Tony Toussaint on 8/27/16.
+//  Copyright © 2016 Tony Toussaint. All rights reserved.
+//
+
+import Foundation
+
+class InterestingCell: UITableViewCell {
+    
+}
