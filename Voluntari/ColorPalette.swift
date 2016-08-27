0ddcdd38929716d0229f6811f93083224e8d3663
tonyyasi <tonyyasi@gmail.com>
2016-08-27 17:01:54 -0500

    Adds colorpalette and UIColor extension to get a color from a hexadecimal

---
+++ Voluntari/ColorPalette.swift
@@ -0,0 +1,13 @@
+//
+//  ColorPalette.swift
+//  Voluntari
+//
+//  Created by Tony Toussaint on 8/27/16.
+//  Copyright © 2016 Tony Toussaint. All rights reserved.
+//
+
+import Foundation
+
+class ColorPalette{
+    static let background = UIColor(red: 0xb9, green: 0xe4, blue: 0xf0)
+}
--- Voluntari/HomeViewController.swift
+++ Voluntari/HomeViewController.swift
@@ -14,6 +14,8 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // b9e4f0
+        view.backgroundColor = ColorPalette.background
 
         // Do any additional setup after loading the view.
     }
